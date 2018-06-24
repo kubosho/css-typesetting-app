d6d92f58554aa39fe8c7ed7b2294ddaf3dcf8309
kubosho <ta2@o2p.jp>
2018-06-24 18:20:26 +0900

    style: auto format to some files

--- bs-config.js
+++ bs-config.js
@@ -1,4 +1,3 @@
-
 /*
  |--------------------------------------------------------------------------
  | Browser-sync config file
@@ -13,101 +12,97 @@
  |
  */
 module.exports = {
-  "ui": {
-    "port": 3001
+  ui: {
+    port: 3001,
   },
-  "files": ['src/**/*.*'],
-  "watchEvents": [
-    'add',
-    'change',
-    'addDir'
-  ],
-  "watch": true,
-  "ignore": [],
-  "single": false,
-  "watchOptions": {
-    "ignoreInitial": true
+  files: ['src/**/*.*'],
+  watchEvents: ['add', 'change', 'addDir'],
+  watch: true,
+  ignore: [],
+  single: false,
+  watchOptions: {
+    ignoreInitial: true,
   },
-  "server": {
+  server: {
     baseDir: 'dist',
-    index: 'index.html'
+    index: 'index.html',
   },
-  "proxy": false,
-  "port": 3000,
-  "middleware": false,
-  "serveStatic": [],
-  "ghostMode": {
-    "clicks": true,
-    "scroll": true,
-    "location": true,
-    "forms": {
-      "submit": true,
-      "inputs": true,
-      "toggles": true
-    }
+  proxy: false,
+  port: 3000,
+  middleware: false,
+  serveStatic: [],
+  ghostMode: {
+    clicks: true,
+    scroll: true,
+    location: true,
+    forms: {
+      submit: true,
+      inputs: true,
+      toggles: true,
+    },
   },
-  "logLevel": "info",
-  "logPrefix": "Browsersync",
-  "logConnections": false,
-  "logFileChanges": true,
-  "logSnippet": true,
-  "rewriteRules": [],
-  "open": "local",
-  "browser": "default",
-  "cors": false,
-  "xip": false,
-  "hostnameSuffix": false,
-  "reloadOnRestart": false,
-  "notify": true,
-  "scrollProportionally": true,
-  "scrollThrottle": 0,
-  "scrollRestoreTechnique": "window.name",
-  "scrollElements": [],
-  "scrollElementMapping": [],
-  "reloadDelay": 0,
-  "reloadDebounce": 500,
-  "reloadThrottle": 0,
-  "plugins": [],
-  "injectChanges": true,
-  "startPath": null,
-  "minify": true,
-  "host": null,
-  "localOnly": false,
-  "codeSync": true,
-  "timestamps": true,
-  "clientEvents": [
-    "scroll",
-    "scroll:element",
-    "input:text",
-    "input:toggles",
-    "form:submit",
-    "form:reset",
-    "click"
+  logLevel: 'info',
+  logPrefix: 'Browsersync',
+  logConnections: false,
+  logFileChanges: true,
+  logSnippet: true,
+  rewriteRules: [],
+  open: 'local',
+  browser: 'default',
+  cors: false,
+  xip: false,
+  hostnameSuffix: false,
+  reloadOnRestart: false,
+  notify: true,
+  scrollProportionally: true,
+  scrollThrottle: 0,
+  scrollRestoreTechnique: 'window.name',
+  scrollElements: [],
+  scrollElementMapping: [],
+  reloadDelay: 0,
+  reloadDebounce: 500,
+  reloadThrottle: 0,
+  plugins: [],
+  injectChanges: true,
+  startPath: null,
+  minify: true,
+  host: null,
+  localOnly: false,
+  codeSync: true,
+  timestamps: true,
+  clientEvents: [
+    'scroll',
+    'scroll:element',
+    'input:text',
+    'input:toggles',
+    'form:submit',
+    'form:reset',
+    'click',
   ],
-  "socket": {
-    "socketIoOptions": {
-      "log": false
+  socket: {
+    socketIoOptions: {
+      log: false,
     },
-    "socketIoClientConfig": {
-      "reconnectionAttempts": 50
+    socketIoClientConfig: {
+      reconnectionAttempts: 50,
     },
-    "path": "/browser-sync/socket.io",
-    "clientPath": "/browser-sync",
-    "namespace": "/browser-sync",
-    "clients": {
-      "heartbeatTimeout": 5000
-    }
+    path: '/browser-sync/socket.io',
+    clientPath: '/browser-sync',
+    namespace: '/browser-sync',
+    clients: {
+      heartbeatTimeout: 5000,
+    },
   },
-  "tagNames": {
-    "less": "link",
-    "scss": "link",
-    "css": "link",
-    "jpg": "img",
-    "jpeg": "img",
-    "png": "img",
-    "svg": "img",
-    "gif": "img",
-    "js": "script"
+  tagNames: {
+    less: 'link',
+    scss: 'link',
+    css: 'link',
+    jpg: 'img',
+    jpeg: 'img',
+    png: 'img',
+    svg: 'img',
+    gif: 'img',
+    js: 'script',
   },
-  "injectNotification": false
+  injectNotification: false,
 };
