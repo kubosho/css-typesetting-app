3bb792bbc2d6cc1c42ae60542ed9f85f7ca3fcb6
kubosho <ta2@o2p.jp>
2018-06-28 22:41:37 +0900

    build: add monaco-editor-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,18 @@
 const path = require('path');
+const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
 const { IS_RELEASE_CHANNEL_PRODUCTION } = require('./config/buildconfig');
 
 const mode = IS_RELEASE_CHANNEL_PRODUCTION ? 'production' : 'development';
+const monacoWebpackPlugin = new MonacoWebpackPlugin();
 
 module.exports = {
   mode,
   entry: './src/index.jsx',
   output: {
-    path: path.resolve(__dirname, 'dist'), // string
+    path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
   },
+  plugins: [monacoWebpackPlugin],
   module: {
     rules: [
       {
